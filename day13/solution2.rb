3d28b2610bc82d13037b83104919e42898a5bfa9
Baine Wedlock <bainewedlock@gmail.com>
2018-12-15 09:27:47 +0100

    refac

--- day13/solution2.rb
+++ day13/solution2.rb
@@ -7,29 +7,19 @@ class TextParser
     end
     puts "remaining cart location: #{@carts[0].pos}"
   end
+
   def tick
-    crashed_carts = []
+    collision_check = CollisionCheck.new(@tracks, @carts)
     @carts.sort_by! do |cart| cart.pos.reverse end
-    occupied = @carts.map(&:pos)
     @carts.each do |cart|
-      occupied.delete(cart.pos)
-      cart.move do |pos, direction|
-        @tracks[pos].possible_directions_for(direction)
-      end
-      if occupied.include? cart.pos
-        other = (@carts - [cart]).find { |test| test.pos == cart.pos }
-        crashed_carts.push cart
-        crashed_carts.push other
-        occupied.delete(other.pos)
-      else
-        occupied.push cart.pos
-      end
+      collision_check.before_move(cart)
+      directions = @tracks[cart.pos].possible_directions_for(cart.direction)
+      cart.move_in_one_of(directions)
+      collision_check.after_move(cart)
     end
-    crashed_carts.each do |cart|
-      @carts -= [cart]
-      break if @carts.size == 1
-    end
+    @carts -= collision_check.crashed_carts
   end
+
   def parse(lines)
     @tracks = {}
     @carts = []
@@ -46,25 +36,54 @@ class TextParser
   end
 end
 
+class CollisionCheck
+  attr_reader :crashed_carts
+  def initialize(tracks, carts)
+    @tracks = tracks
+    @carts = carts
+    @occupied = carts.map(&:pos)
+    @crashed_carts = []
+  end
+
+  def before_move(cart)
+    @occupied.delete cart.pos
+  end
+
+  def after_move(cart)
+    if @occupied.include? cart.pos
+      other = (@carts - [cart]).find { |test| test.pos == cart.pos }
+      @occupied.delete other.pos
+      @crashed_carts.push cart
+      @crashed_carts.push other
+    else
+      @occupied.push cart.pos
+    end
+  end
+end
+
 class Track
   def self.try_parse(char)
     case char
-    when '-', '<', '>' then yield create_with(:left, :left, :right, :right)
-    when '|', '^', 'v' then yield create_with(:up, :up, :down, :down)
-    when '/' then yield create_with(:left, :down, :up, :right)
-    when '\\' then yield create_with(:right, :down, :up, :left)
-    when '+' then yield Intersection.new()
+    when '-', '<', '>' then yield with_routes(:left,  :left, :right, :right)
+    when '|', '^', 'v' then yield with_routes(:up,    :up,   :down,  :down)
+    when '/'           then yield with_routes(:left,  :down, :up,    :right)
+    when '\\'          then yield with_routes(:right, :down, :up,    :left)
+    when '+'           then yield Intersection.new
     end
   end
-  def self.create_with(*directions)
-    Track.new(directions.each_slice(2).map do |from, to|
+
+  def self.with_routes(*directions)
+    Track.new(directions.each_slice(2).flat_map do |from, to|
       [[from, to], [to, from]]
-    end.flatten(1).to_h)
+    end.to_h)
   end
+
   def possible_directions_for(direction)
     [@map[direction]]
   end
+
   private
+
   def initialize(map)
     @map = map
   end
@@ -72,33 +91,38 @@ end
 
 class Intersection
   def possible_directions_for(direction)
-    circle = [:left, :up, :right, :down] * 3
-    current = circle.index(direction) + 4
-    circle.drop(current - 1).take(3)
+    circle = %i[left up right down] * 3
+    start = circle.index(direction) + 4
+    circle.drop(start - 1).take(3)
   end
 end
 
 class Cart
   attr_reader :pos
+  attr_reader :direction
   def self.try_parse(x, y, char)
     case char
-    when '<' then yield Cart.new(x, y, :left )
-    when '^' then yield Cart.new(x, y, :up   )
-    when 'v' then yield Cart.new(x, y, :down )
+    when '<' then yield Cart.new(x, y, :left)
+    when '^' then yield Cart.new(x, y, :up)
+    when 'v' then yield Cart.new(x, y, :down)
     when '>' then yield Cart.new(x, y, :right)
     end
   end
-  def move
-    @direction = choose_from yield(@pos, @direction)
+
+  def move_in_one_of(directions)
+    @direction = choose_from(directions)
     @pos = @pos.send(@direction)
   end
+
   def choose_from(directions)
     return directions[0] if directions.size == 1
     choice = directions[@next_intersection_choice]
     @next_intersection_choice = (@next_intersection_choice + 1) % 3
-    return choice
+    choice
   end
+
   private
+
   def initialize(x, y, direction)
     @pos = [x, y]
     @direction = direction
@@ -110,12 +134,15 @@ class Array
   def down
     [self[0], self[1] + 1]
   end
+
   def up
     [self[0], self[1] - 1]
   end
+
   def left
     [self[0] - 1, self[1]]
   end
+
   def right
     [self[0] + 1, self[1]]
   end
